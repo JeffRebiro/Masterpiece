345a1afe5a554358ea16203a3aeb07d37a29e431
JeffRebiro <jeffrebiro@gmail.com>
2025-10-07 17:05:13 +0300

    Deployment

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,7 @@ export const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST});
 
-        const { data } = await axios.get('http://localhost:5000/api/products');
+        const { data } = await axios.get('https://masterpiece-lsvl.onrender.com/api/products');
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
     } catch (err) {
         dispatch({type: PRODUCT_LIST_FAIL, payload: err.response && err.response.data.message ?err.response.data.message: err.message, })
@@ -15,7 +15,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`http://localhost:5000/api/products/${id}`);
+        const { data } = await axios.get(`https://masterpiece-lsvl.onrender.com/api/products/${id}`);
         console.log("Fetched product:", data); // should log the full product object
         dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
